d2af524eb5c91f3d022bae2851ccaa769675aa7c
hedgehog0213 <rladudtj52@naver.com>
2023-12-06 09:53:38 +0900

    이쁘게

--- be/src/apis/reservations/dto/createRsv.input.ts
+++ be/src/apis/reservations/dto/createRsv.input.ts
@@ -1,32 +1,39 @@
-import { IsArray, IsDate, IsDateString, IsInt, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import {
+  IsArray,
+  IsDate,
+  IsDateString,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateReservationInfoInput {
-@IsString()
-@IsNotEmpty()
-bg_color:string;
+  @IsString()
+  @IsNotEmpty()
+  bg_color: string;
 
-@IsString()
-@IsNotEmpty()
-rsvr_id:string;
+  @IsString()
+  @IsNotEmpty()
+  rsvr_id: string;
 
-@IsNotEmpty()
-@IsDateString()
-rsvt_start_dt:Date;
+  @IsNotEmpty()
+  @IsDateString()
+  rsvt_start_dt: Date;
 
-@IsDateString()
-@IsNotEmpty()
-rsvt_end_dt:Date;
+  @IsDateString()
+  @IsNotEmpty()
+  rsvt_end_dt: Date;
 
+  @IsNotEmpty()
+  @IsString()
+  rsvt_purpose: string;
 
-@IsNotEmpty()
-@IsString()
-rsvt_purpose:string;
+  @IsInt()
+  @IsNotEmpty()
+  rsvt_rm_no: number;
 
-@IsInt()
-@IsNotEmpty()
-rsvt_rm_no:number;
-
-@IsArray()
-@IsOptional()
-other_ptcp?:string[];
+  @IsArray()
+  @IsOptional()
+  other_ptcp?: string[];
 }
--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,26 +1,36 @@
-import { Module } from "@nestjs/common";
-import { ReservationsModule } from "./apis/reservations/reservations.module";
-import { UsersModule } from "./apis/user/users.module";
-import { GraphQLModule } from "@nestjs/graphql";
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-import { DbcModule } from "./apis/dbc/dbc.module";
-import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AuthModule } from "./apis/auth/auth.module";
-import { WinstonModule, utilities } from 'nest-winston'
-import winston from "winston";
+import { Module } from '@nestjs/common';
+import { ReservationsModule } from './apis/reservations/reservations.module';
+import { UsersModule } from './apis/user/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DbcModule } from './apis/dbc/dbc.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './apis/auth/auth.module';
+import { WinstonModule, utilities } from 'nest-winston';
+import winston from 'winston';
 @Module({
-    imports:[
-        ConfigModule.forRoot({isGlobal: true, envFilePath : './.env'}),
-        // GraphQLModule.forRoot<ApolloDriverConfig>({
-        //     driver: ApolloDriver,
-        //     autoSchemaFile: "src/commons/graphql/schema.gql"
-        // }),
-        ReservationsModule, UsersModule, DbcModule,TypeOrmModule.forRoot({
-            type:'mariadb',host:process.env.DATABASE_HOST,port:Number(process.env.DATABASE_PORT),username:process.env.DATABASE_USERNAME,password:process.env.DATABASE_PASSWORD,database:process.env.DATABASE_DATABASE,entities:[__dirname + '/apis/**/*.entity.*'],logging:true,synchronize:true
-        }),
-    AuthModule],
-
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //     driver: ApolloDriver,
+    //     autoSchemaFile: "src/commons/graphql/schema.gql"
+    // }),
+    ReservationsModule,
+    UsersModule,
+    DbcModule,
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      logging: true,
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
 })
-
-export class AppModule{}
+export class AppModule {}
